28857d5fd844d025ee34e6c0b4972e50e7c71750
Artur Belousov <g.infosecurity@gmail.com>
2015-04-09 13:29:29 +0300

    added arichmetiks and some tests

---
+++ src/main/java/ua/com/pb/biplane/testexercise/bl/OperationsCheck.java
@@ -0,0 +1,7 @@
+package ua.com.pb.biplane.testexercise.bl;
+
+/**
+ * Created by artur on 09.04.15.
+ */
+public class OperationsCheck {
+}
---
+++ src/test/java/ua/com/pb/biplane/testexercise/bl/OperationsCheckTest.java
@@ -0,0 +1,21 @@
+package ua.com.pb.biplane.testexercise.bl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by artur on 09.04.15.
+ */
+public class OperationsCheckTest {
+
+    @Test
+    public void testSumNumbers() throws Exception {
+
+    }
+
+    @Test
+    public void testSumStrings() throws Exception {
+
+    }
+}
